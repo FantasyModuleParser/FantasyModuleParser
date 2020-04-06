fb396fca22ee4f9dd3a8b22d1b650fa694136120
Tim Langen <tim.langen@driessen.nl>
2020-04-06 09:57:11 +0200

    chore(rework): Completely reworked the directories and completely removed any reference to "WpfApp1"

---
+++ EngineeringSuite/ESEditDeleteNPC.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace EngineeringSuite
+{
+    /// <summary>
+    /// Interaction logic for ESEditDeleteNPC.xaml
+    /// </summary>
+    public partial class ESEditDeleteNPC : Window
+    {
+        public ESEditDeleteNPC()
+        {
+            InitializeComponent();
+        }
+        private void ESExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
