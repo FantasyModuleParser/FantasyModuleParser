11e06af31cef0d5ec96f755619240d263ac07f39
EngineerSuite2 <62843245+EngineerSuite2@users.noreply.github.com>
2020-04-26 06:36:34 -0700

    1) Added NPCDataModel.cs 2) Using NPCDataModel for JSON file creation 3) Started writing C# logic in NPCEngineer.xaml.cs

---
+++ EngineeringSuite/NPC/NPCDataModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngineeringSuite.NPC
+{
+    public class NPCDataModel
+    {
+        public string NPCName { get; set; }
+        public string Size { get; set; }
+        public string NPCType { get; set; } 
+        public string Tag { get; set; }
+        public string Alignment { get; set; }
+        public string AC { get; set; }
+        public string HP { get; set; }
+        public string NPCGender { get; set; }
+        public bool Unique { get; set; }
+        public bool NPCNamed { get; set; }
+        public string Speed { get; set; }
+        public string Burrow { get; set; }
+        public string Climb { get; set; }
+        public string Fly { get; set; }
+        public bool Hover { get; set; }
+        public string Swim { get; set; }
+    }
+}
