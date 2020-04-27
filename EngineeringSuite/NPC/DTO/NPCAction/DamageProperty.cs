39e47f5aba8a7b26faf251635d5a791ace10d654
Fskallos <fredska@gmail.com>
2020-04-26 22:37:02 -0400

    NPC Action DTO code in place; Mutliattack Update button functions;

---
+++ EngineeringSuite/NPC/DTO/NPCAction/DamageProperty.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngineeringSuite.NPC.DTO.NPCAction
+{
+    class DamageProperty
+    {
+        int numOfDice;
+        int dieType; // valid values:  4, 6, 8, 10, 12, 20
+        int bonus;
+        String damageType;
+    }
+}
