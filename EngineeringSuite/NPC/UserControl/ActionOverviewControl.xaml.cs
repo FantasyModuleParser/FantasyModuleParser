5e219b88ca9a907e40c8335d954b1ed094093736
Fskallos <fredska@gmail.com>
2020-04-29 23:20:18 -0400

    Created UserControl for Action summary on NPCEActions page

---
+++ EngineeringSuite/NPC/UserControl/ActionOverviewControl.xaml.cs
@@ -0,0 +1,56 @@
+﻿using EngineeringSuite.NPC.Models.NPCAction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EngineeringSuite.NPC.UserController
+{
+    /// <summary>
+    /// Interaction logic for ActionOverviewControl.xaml
+    /// </summary>
+    public partial class ActionOverviewControl : UserControl
+    {
+
+        public ActionOverviewControl()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public string AOCId { get; set; }
+
+        public int Id { get; set; }
+
+
+        private void btn_Up_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Up Button Pressed for Action Overview Control :: ID = " + AOCId);
+        }
+
+        private void btn_Down_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Down Button Pressed for Action Overview Control :: ID = " + AOCId);
+        }
+
+        private void btn_Edit_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Edit Button Pressed for Action Overview Control :: ID = " + AOCId);
+        }
+
+        private void btn_Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Cancel Button Pressed for Action Overview Control :: ID = " + AOCId);
+        }
+    }
+}
