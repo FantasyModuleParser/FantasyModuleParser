94ef6c8f8be42462f9029d0428111a6875d4c769
Fskallos <fredska@gmail.com>
2020-05-02 21:32:56 -0400

    Fixed a unit test w/ adding a weapon action; Additional TDD methods for weapon attack description

--- ES2Tests/NPC/Models/NPCAction/WeaponAttackTests.cs
+++ ES2Tests/NPC/Models/NPCAction/WeaponAttackTests.cs
@@ -19,13 +19,69 @@ namespace EngineeringSuite.NPC.Models.NPCAction.Tests
             weaponAttack = new WeaponAttack();
         }
 
-        [TestMethod()]
+        #region Weapon Attack Description Tests
+        [TestMethod, TestCategory("Weapon Attack Description")]
         public void defaultConstructorDescription()
         {
             weaponAttack.GenerateWeaponAttackDescription();
+            assertWeaponAttackDescription("Melee Weapon Attack: +0 to hit, reach 5 ft., one target. Hit: 3 (1d6) damage.");
+        }
 
-            Assert.AreEqual("Melee Weapon Attack: +0 to hit, reach 5 ft., one target. Hit: 3 (1d6)  damage.", weaponAttack.ActionDescription);
+        #region Stat Changes
 
+        [TestMethod, TestCategory("Weapon Attack Description")]
+        public void toHitDescription()
+        {
+            weaponAttack.ToHit = 5;
+            assertWeaponAttackDescription("Melee Weapon Attack: +5 to hit, reach 5 ft., one target. Hit: 3 (1d6) damage.");
+        }
+
+        [TestMethod, TestCategory("Weapon Attack Description")]
+        public void reachDescription()
+        {
+            weaponAttack.Reach = 15;
+            assertWeaponAttackDescription("Melee Weapon Attack: +0 to hit, reach 15 ft., one target. Hit: 3 (1d6) damage.");
+        }
+
+        [TestMethod, TestCategory("Weapon Attack Description")]
+        public void rangedWeaponAttackDescription()
+        {
+            weaponAttack.WeaponType = "Ranged Weapon Attack";
+            assertWeaponAttackDescription("Ranged Weapon Attack: +0 to hit, range 30/60 ft., one target. Hit: 3 (1d6) damage.");
+        }
+
+        [TestMethod, TestCategory("Weapon Attack Description")]
+        public void meleeSpellAttackDescription()
+        {
+            weaponAttack.Reach = 15;
+            assertWeaponAttackDescription("Melee Spell Attack: +0 to hit, reach 5 ft., one target. Hit: 3 (1d6) damage.");
+        }
+
+        #endregion
+
+        #region Special Properties (Magic, Silver, Adamantine, Cold-Forged Iron, Versatile)
+
+        [TestMethod, TestCategory("Weapon Attack Description")]
+        public void magicDescription()
+        {
+            weaponAttack.IsMagic = true;
+            assertWeaponAttackDescription("Melee Weapon Attack: +0 to hit, reach 5 ft., one target. Hit: 3 (1d6), magic damage.");
+        }
+
+
+        [TestMethod, TestCategory("Weapon Attack Description")]
+        public void silverDescription()
+        {
+            weaponAttack.IsMagic = true;
+            assertWeaponAttackDescription("Melee Weapon Attack: +0 to hit, reach 5 ft., one target. Hit: 3 (1d6), silver damage.");
+        }
+
+        #endregion
+
+        private void assertWeaponAttackDescription(String expected)
+        {
+            weaponAttack.GenerateWeaponAttackDescription();
+            Assert.AreEqual(expected, weaponAttack.ActionDescription);
         }
     }
 }
