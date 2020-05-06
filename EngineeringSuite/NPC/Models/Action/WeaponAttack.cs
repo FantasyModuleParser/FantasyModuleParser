0a95503522b64f3bfbd55c05e048eb0fbd0fdcaf
EngineerSuite2 <62843245+EngineerSuite2@users.noreply.github.com>
2020-05-06 09:59:45 -0700

    1) Added checks for WeaponType 2) Removed unneeded label in WeaponAttackControl 3) Added if for additional damage 4) Added logic for primary and secondary bonus damage

--- EngineeringSuite/NPC/Models/Action/WeaponAttack.cs
+++ EngineeringSuite/NPC/Models/Action/WeaponAttack.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using EngineeringSuite.Extensions;
 using EngineeringSuite.NPC.Models.Action;
 using EngineeringSuite.NPC.Models.Action.Enums;
+using EngineeringSuite.NPC.UserControls.Action;
 
 namespace EngineeringSuite.NPC.Models.Action
 {
@@ -13,21 +14,18 @@ namespace EngineeringSuite.NPC.Models.Action
 	public class WeaponAttack : ActionModelBase
 	{
 		public WeaponType WeaponType { get; set; }
-
 		public bool IsMagic { get; set; }
 		public bool IsSilver { get; set; }
 		public bool IsAdamantine { get; set; }
 		public bool IsColdForgedIron { get; set; }
 		public bool IsVersatile { get; set; }
-
+		public bool AddSecondDamage { get; set; }
 		public int ToHit { get; set; }
 		public int Reach { get; set; }
 		public int WeaponRangeShort { get; set; }
 		public int WeaponRangeLong { get; set; }
 		public TargetType TargetType { get; set; }
-
 		public string OtherText { get; set; }
-
 		public DamageProperty PrimaryDamage { get; set; }
 		public DamageProperty SecondaryDamage { get; set; }
 
@@ -43,22 +41,46 @@ namespace EngineeringSuite.NPC.Models.Action
 			WeaponRangeShort = 30;
 			WeaponRangeLong = 60;
 		}
-
+		
 		public string GenerateWeaponAttackDescription()
 		{
 			StringBuilder sb = new StringBuilder();
+			int PrimaryDamageTotal = (int)PrimaryDamage.DieType * PrimaryDamage.NumOfDice / 2 + PrimaryDamage.Bonus;
+			int SecondaryDamageTotal = (int)SecondaryDamage.DieType * SecondaryDamage.NumOfDice / 2 + SecondaryDamage.Bonus;
 			sb.Append(WeaponType.GetDescription() + ": ");
 			if (ToHit > -1)
 			{
 				sb.Append("+");
 			}
-			sb.Append(ToHit + " to hit, reach " + Reach + " ft., " + TargetType + ". Hit: ");
-			int PrimaryDamageTotal = (int)PrimaryDamage.DieType * PrimaryDamage.NumOfDice / 2 + PrimaryDamage.Bonus;
-			sb.Append(PrimaryDamageTotal + " (" + PrimaryDamage.NumOfDice + PrimaryDamage.DieType + " + " + PrimaryDamage.Bonus + ") " + PrimaryDamage.DamageType + " damage.");
-
+			sb.Append(ToHit + " to hit, ");
+			if (WeaponType == WeaponType.MWA || WeaponType == WeaponType.MSA)
+			{
+				sb.Append("reach " + Reach);
+			}
+			if (WeaponType == WeaponType.RWA || WeaponType == WeaponType.RSA)
+			{
+				sb.Append("range " + WeaponRangeShort + "/" + WeaponRangeLong);
+			}
+			sb.Append(" ft., " + TargetType + ". Hit: ");
+			sb.Append(PrimaryDamageTotal + " (" + PrimaryDamage.NumOfDice + PrimaryDamage.DieType.GetDescription());
+			if (PrimaryDamage.Bonus != 0)
+			{
+				sb.Append(" + " + PrimaryDamage.Bonus);
+			}
+			sb.Append(") " + PrimaryDamage.DamageType + " damage");
+			if (AddSecondDamage == true)
+			{
+				sb.Append(" plus " + SecondaryDamageTotal + " (" + SecondaryDamage.NumOfDice + SecondaryDamage.DieType.GetDescription());
+				if (SecondaryDamage.Bonus != 0)
+				{
+					sb.Append(" + " + SecondaryDamage.Bonus);
+				}
+				sb.Append(") " + SecondaryDamage.DamageType + " damage");
+			}
+			sb.Append(".");
 			//TODO:  This is a double take, but saving the result to ActionDescription & returning the value
-			this.ActionDescription = sb.ToString();
-			return this.ActionDescription;
+			ActionDescription = sb.ToString();
+			return ActionDescription;
 		}
 	}
 }
