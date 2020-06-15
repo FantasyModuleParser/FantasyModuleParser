d4a4428a276434c3894d1193a03dbfdcb4fbfea2
Fskallos <fredska@gmail.com>
2020-06-14 21:50:40 -0400

    Non-dynamic Preview window with Speed description

---
+++ FantasyModuleParser/NPC/ViewModels/PreviewNPCViewModel.cs
@@ -0,0 +1,31 @@
+﻿using FantasyModuleParser.NPC.Controllers;
+using FantasyModuleParser.NPC.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FantasyModuleParser.NPC.ViewModels
+{
+
+    public class PreviewNPCViewModel : ViewModelBase
+    {
+        public NPCModel NPCModel { get; set; }
+        private NPCController npcController;
+
+        public string SpeedDescription { get; set; }
+
+        public PreviewNPCViewModel()
+        {
+            npcController = new NPCController();
+            NPCModel = npcController.GetNPCModel();
+            SpeedDescription = UpdateSpeedDescription();
+        }
+
+        public string UpdateSpeedDescription()
+        {
+            return "Speed: " + NPCModel.Speed + ", Fly: " + NPCModel.Fly + ", Burrow: " + NPCModel.Burrow;
+        }
+    }
+}
