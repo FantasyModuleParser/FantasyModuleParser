fcbed809febddb5cd3189bf01d40306ef13a44fd
Fskallos <fredska@gmail.com>
2020-06-15 22:29:28 -0400

    Modified unit tests to be more in sync with existing code

--- FMPTests/NPC/ViewModels/PreviewNPCViewModelTests.cs
+++ FMPTests/NPC/ViewModels/PreviewNPCViewModelTests.cs
@@ -28,15 +28,15 @@ namespace FantasyModuleParser.NPC.ViewModels.Tests
         [DataRow(30, 0, 0, 30, false, 0, "30 ft., fly 30 ft.")]
         [DataRow(30, 0, 0, 30, true, 0, "30 ft., fly 30 ft. (hover)")]
         [DataRow(30, 0, 0, 0, false, 30, "30 ft., swim 30 ft.")]
-        [DataRow(30, 30, 30, 0, false, 0, "30 ft., burrow 30 ft., climb 30 ft.")]
-        [DataRow(30, 30, 0, 30, false, 0, "30 ft., burrow 30 ft., fly 30 ft.")]
-        [DataRow(30, 30, 0, 30, true, 0, "30 ft., burrow 30 ft., fly 30 ft. (hover)")]
+        [DataRow(30, 30, 30, 0, false, 0, "30 ft., climb 30 ft., burrow 30 ft.")]
+        [DataRow(30, 30, 0, 30, false, 0, "30 ft., fly 30 ft., burrow 30 ft.")]
+        [DataRow(30, 30, 0, 30, true, 0, "30 ft., fly 30 ft. (hover), burrow 30 ft.")]
         [DataRow(30, 30, 0, 0, false, 30, "30 ft., burrow 30 ft., swim 30 ft.")]
-        [DataRow(30, 30, 30, 30, false, 0, "30 ft., burrow 30 ft., climb 30 ft., fly 30 ft.")]
-        [DataRow(30, 30, 30, 30, true, 0, "30 ft., burrow 30 ft., climb 30 ft., fly 30 ft. (hover)")]
-        [DataRow(30, 30, 30, 0, false, 30, "30 ft., burrow 30 ft., climb 30 ft., swim 30 ft.")]
-        [DataRow(30, 30, 30, 30, false, 30, "30 ft., burrow 30 ft., climb 30 ft., fly 30 ft., swim 30 ft.")]
-        [DataRow(30, 30, 30, 30, true, 30, "30 ft., burrow 30 ft., climb 30 ft., fly 30 ft. (hover), swim 30 ft.")]
+        [DataRow(30, 30, 30, 30, false, 0, "30 ft., climb 30 ft., fly 30 ft., burrow 30 ft.")]
+        [DataRow(30, 30, 30, 30, true, 0, "30 ft., climb 30 ft., fly 30 ft. (hover), burrow 30 ft.")]
+        [DataRow(30, 30, 30, 0, false, 30, "30 ft., climb 30 ft., burrow 30 ft., swim 30 ft.")]
+        [DataRow(30, 30, 30, 30, false, 30, "30 ft., climb 30 ft., fly 30 ft., burrow 30 ft., swim 30 ft.")]
+        [DataRow(30, 30, 30, 30, true, 30,  "30 ft., climb 30 ft., fly 30 ft. (hover), burrow 30 ft., swim 30 ft.")]
         public void UpdateSpeedDescriptionTest(int speed, int burrow, int climb, int fly, bool hover, int swim, string expected)
         {
             _npcModel = new NPCModel();
