19dbfd4a7cbcab4a93281acf669fb17e64901077
EngineerSuite2 <62843245+EngineerSuite2@users.noreply.github.com>
2020-06-16 15:45:51 -0700

    Renamed "sl / sb" to "stringBuilder"

--- FantasyModuleParser/NPC/ViewModels/PreviewNPCViewModel.cs
+++ FantasyModuleParser/NPC/ViewModels/PreviewNPCViewModel.cs
@@ -34,19 +34,20 @@ namespace FantasyModuleParser.NPC.ViewModels
 
         public string UpdateSpeedDescription()
         {
-            StringBuilder sl = new StringBuilder();
+            StringBuilder stringBuilder
+                = new StringBuilder();
 
             if(NPCModel.Speed == 0)
-                sl.Append("0 ft., ");
+                stringBuilder.Append("0 ft., ");
             else 
-                sl.Append(_appendSpeedAttribute("", NPCModel.Speed, false));
-            sl.Append(_appendSpeedAttribute("climb", NPCModel.Climb, false));
-            sl.Append(_appendSpeedAttribute("fly", NPCModel.Fly, NPCModel.Hover));
-            sl.Append(_appendSpeedAttribute("burrow", NPCModel.Burrow, false));
-            sl.Append(_appendSpeedAttribute("swim", NPCModel.Swim, false));
+                stringBuilder.Append(_appendSpeedAttribute("", NPCModel.Speed, false));
+                stringBuilder.Append(_appendSpeedAttribute("climb", NPCModel.Climb, false));
+                stringBuilder.Append(_appendSpeedAttribute("fly", NPCModel.Fly, NPCModel.Hover));
+                stringBuilder.Append(_appendSpeedAttribute("burrow", NPCModel.Burrow, false));
+                stringBuilder.Append(_appendSpeedAttribute("swim", NPCModel.Swim, false));
 
-            sl.Remove(sl.Length - 2, 2);
-            return sl.ToString().Trim();
+            stringBuilder.Remove(stringBuilder.Length - 2, 2);
+            return stringBuilder.ToString().Trim();
         }
 
         private string _appendSpeedAttribute(string name, int value, bool hover)
@@ -58,30 +59,30 @@ namespace FantasyModuleParser.NPC.ViewModels
 
         private string _updateSkillsDescription()
         {
-            StringBuilder sb = new StringBuilder();
+            StringBuilder stringBuilder = new StringBuilder();
             string delimiter = ", ";
-            sb.Append(appendSkill("Acrobatics", NPCModel.Acrobatics)).Append(delimiter);
-            sb.Append(appendSkill("Animal Handling", NPCModel.AnimalHandling)).Append(delimiter);
-            sb.Append(appendSkill("Arcana", NPCModel.Arcana)).Append(delimiter);
-            sb.Append(appendSkill("Athletics", NPCModel.Athletics)).Append(delimiter);
-            sb.Append(appendSkill("Deception", NPCModel.Deception)).Append(delimiter);
-            sb.Append(appendSkill("History", NPCModel.History)).Append(delimiter);
-            sb.Append(appendSkill("Insight", NPCModel.Insight)).Append(delimiter);
-            sb.Append(appendSkill("Intimidation", NPCModel.Intimidation)).Append(delimiter);
-            sb.Append(appendSkill("Investigation", NPCModel.Investigation)).Append(delimiter);
-            sb.Append(appendSkill("Medicine", NPCModel.Medicine)).Append(delimiter);
-            sb.Append(appendSkill("Nature", NPCModel.Nature)).Append(delimiter);
-            sb.Append(appendSkill("Perception", NPCModel.Perception)).Append(delimiter);
-            sb.Append(appendSkill("Performance", NPCModel.Performance)).Append(delimiter);
-            sb.Append(appendSkill("Persuasion", NPCModel.Persuasion)).Append(delimiter);
-            sb.Append(appendSkill("Religion", NPCModel.Religion)).Append(delimiter);
-            sb.Append(appendSkill("Sleight of Hand", NPCModel.SleightOfHand)).Append(delimiter);
-            sb.Append(appendSkill("Stealth", NPCModel.Stealth)).Append(delimiter);
-            sb.Append(appendSkill("Survival", NPCModel.Survival)).Append(delimiter);
+            stringBuilder.Append(appendSkill("Acrobatics", NPCModel.Acrobatics)).Append(delimiter);
+            stringBuilder.Append(appendSkill("Animal Handling", NPCModel.AnimalHandling)).Append(delimiter);
+            stringBuilder.Append(appendSkill("Arcana", NPCModel.Arcana)).Append(delimiter);
+            stringBuilder.Append(appendSkill("Athletics", NPCModel.Athletics)).Append(delimiter);
+            stringBuilder.Append(appendSkill("Deception", NPCModel.Deception)).Append(delimiter);
+            stringBuilder.Append(appendSkill("History", NPCModel.History)).Append(delimiter);
+            stringBuilder.Append(appendSkill("Insight", NPCModel.Insight)).Append(delimiter);
+            stringBuilder.Append(appendSkill("Intimidation", NPCModel.Intimidation)).Append(delimiter);
+            stringBuilder.Append(appendSkill("Investigation", NPCModel.Investigation)).Append(delimiter);
+            stringBuilder.Append(appendSkill("Medicine", NPCModel.Medicine)).Append(delimiter);
+            stringBuilder.Append(appendSkill("Nature", NPCModel.Nature)).Append(delimiter);
+            stringBuilder.Append(appendSkill("Perception", NPCModel.Perception)).Append(delimiter);
+            stringBuilder.Append(appendSkill("Performance", NPCModel.Performance)).Append(delimiter);
+            stringBuilder.Append(appendSkill("Persuasion", NPCModel.Persuasion)).Append(delimiter);
+            stringBuilder.Append(appendSkill("Religion", NPCModel.Religion)).Append(delimiter);
+            stringBuilder.Append(appendSkill("Sleight of Hand", NPCModel.SleightOfHand)).Append(delimiter);
+            stringBuilder.Append(appendSkill("Stealth", NPCModel.Stealth)).Append(delimiter);
+            stringBuilder.Append(appendSkill("Survival", NPCModel.Survival)).Append(delimiter);
 
             // Remove the last comma
-            sb.Remove(sb.Length - 2, 2);
-            return sb.ToString();
+            stringBuilder.Remove(stringBuilder.Length - 2, 2);
+            return stringBuilder.ToString();
         }
 
         private string appendSkill(string skillName, int skillValue)
