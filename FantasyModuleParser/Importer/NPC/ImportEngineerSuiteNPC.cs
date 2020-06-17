7e415306a46d266f78b58e85b7f888e4529e52b4
Fskallos <fredska@gmail.com>
2020-06-16 21:44:43 -0400

    Added Engineer Suite test files for Unit testing

---
+++ FantasyModuleParser/Importer/NPC/ImportEngineerSuiteNPC.cs
@@ -0,0 +1,23 @@
+﻿using FantasyModuleParser.NPC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FantasyModuleParser.Importer.NPC
+{
+    public class ImportEngineerSuiteNPC
+    {
+        public ImportEngineerSuiteNPC()
+        {
+
+        }
+
+        public NPCModel ParseEngineerSuiteNPCContent(string esNpcFileContent)
+        {
+            NPCModel parsedNPCModel = new NPCModel();
+            return parsedNPCModel;
+        }
+    }
+}
