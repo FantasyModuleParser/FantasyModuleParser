06d95f05ff986b197107c44cb09836b1923208d5
Fskallos <fredska@gmail.com>
2020-06-17 22:25:13 -0400

    Added unit tests for Skills tab

--- FMPTests/Importer/NPC/ImportEngineerSuiteNPCTests.cs
+++ FMPTests/Importer/NPC/ImportEngineerSuiteNPCTests.cs
@@ -12,6 +12,9 @@ using FantasyModuleParser.NPC;
 using FantasyModuleParser.NPC.Models.Action;
 using FantasyModuleParser.NPC.Controllers;
 using FantasyModuleParser.NPC.Models.Action.Enums;
+using FantasyModuleParser.Extensions;
+using System.Collections.ObjectModel;
+using FantasyModuleParser.NPC.Models.Skills;
 
 namespace FantasyModuleParser.Importer.NPC.Tests
 {
@@ -31,7 +34,7 @@ namespace FantasyModuleParser.Importer.NPC.Tests
             using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("FMPTests.Resources.V1_npc_basestats.npc"))
             using (StreamReader reader = new StreamReader(stream))
             {
-                 return reader.ReadToEnd();
+                return reader.ReadToEnd();
             }
         }
 
@@ -190,14 +193,14 @@ namespace FantasyModuleParser.Importer.NPC.Tests
             }
         }
         [TestMethod()]
-        public void BaseStats_DamageVulnerabilities()
+        public void Resistances_DamageVulnerabilities()
         {
             NPCController controller = new NPCController();
             List<SelectableActionModel> expectedDamageVulnerabilites = controller.GetSelectableActionModelList(typeof(DamageType));
 
             // For the unit test, the following are set to true based on the file v1_npc_all.npc
             // Acid, Fire, Lighting, Poison, Radiant, Bludgeoning, Slashing
-            foreach(SelectableActionModel selectableActionModel in expectedDamageVulnerabilites)
+            foreach (SelectableActionModel selectableActionModel in expectedDamageVulnerabilites)
             {
                 switch (selectableActionModel.ActionName)
                 {
@@ -217,7 +220,7 @@ namespace FantasyModuleParser.Importer.NPC.Tests
         }
 
         [TestMethod()]
-        public void BaseStats_DamageResistances()
+        public void Resistances_DamageResistances()
         {
             NPCController controller = new NPCController();
             List<SelectableActionModel> expectedDamageResistances = controller.GetSelectableActionModelList(typeof(DamageType));
@@ -237,8 +240,220 @@ namespace FantasyModuleParser.Importer.NPC.Tests
             }
 
             Assert.AreEqual(expectedDamageResistances, LoadEngineerSuiteTestNPCFile().DamageResistanceModelList);
+
+        }
+
+        [TestMethod()]
+        public void Resistances_SpecialWeaponResistance()
+        {
+            NPCController controller = new NPCController();
+            List<SelectableActionModel> expectedWeaponResistance = controller.GetSelectableActionModelList(typeof(WeaponResistance));
+
+            foreach (SelectableActionModel selectableActionModel in expectedWeaponResistance)
+            {
+                switch (selectableActionModel.ActionName)
+                {
+                    case "Resistant to nonmagical weapons":
+                        selectableActionModel.Selected = true;
+                        break;
+                }
+            }
+
+            Assert.AreEqual(expectedWeaponResistance, LoadEngineerSuiteTestNPCFile().SpecialWeaponResistanceModelList);
+        }
+
+        [TestMethod()]
+        public void Resistances_DamageImmunities()
+        {
+            NPCController controller = new NPCController();
+            List<SelectableActionModel> expectedActionModelList = controller.GetSelectableActionModelList(typeof(DamageType));
+
+            foreach (SelectableActionModel selectableActionModel in expectedActionModelList)
+            {
+                switch (selectableActionModel.ActionName)
+                {
+                    case "Acid":
+                    case "Force":
+                    case "Poison":
+                    case "Thunder":
+                    case "Slashing":
+                        selectableActionModel.Selected = true;
+                        break;
+                }
+            }
+
+            Assert.AreEqual(expectedActionModelList, LoadEngineerSuiteTestNPCFile().DamageImmunityModelList);
+
+        }
+
+        [TestMethod()]
+        public void Resistances_SpecialWeaponImmunity()
+        {
+            NPCController controller = new NPCController();
+            List<SelectableActionModel> expectedActionModelList = controller.GetSelectableActionModelList(typeof(WeaponImmunity));
+
+            foreach (SelectableActionModel selectableActionModel in expectedActionModelList)
+            {
+                switch (selectableActionModel.ActionName)
+                {
+                    case "Immune to nonmagical weapons that aren't silvered":
+                        selectableActionModel.Selected = true;
+                        break;
+                }
+            }
+
+            Assert.AreEqual(expectedActionModelList, LoadEngineerSuiteTestNPCFile().SpecialWeaponImmunityModelList);
+        }
+
+        [TestMethod()]
+        public void Resistances_ConditionImmunities()
+        {
+            NPCController controller = new NPCController();
+            List<SelectableActionModel> expectedActionModelList = controller.GetSelectableActionModelList(typeof(ConditionType));
+
+            foreach (SelectableActionModel selectableActionModel in expectedActionModelList)
+            {
+                switch (selectableActionModel.ActionName)
+                {
+                    case "Blinded":
+                    case "Frightened":
+                    case "Invisible":
+                    case "Paralyzed":
+                    case "Prone":
+                    case "Restrained":
+                        selectableActionModel.Selected = true;
+                        break;
+                }
+            }
+
+            Assert.AreEqual(expectedActionModelList, LoadEngineerSuiteTestNPCFile().ConditionImmunityModelList);
         }
 
         #endregion
+
+        #region Skills
+
+        [TestMethod()]
+        public void Skills_Bonuses()
+        {
+            NPCModel actualNPCModel = LoadEngineerSuiteTestNPCFile();
+
+            Assert.AreEqual(1, actualNPCModel.Acrobatics);
+            Assert.AreEqual(2, actualNPCModel.AnimalHandling);
+            Assert.AreEqual(3, actualNPCModel.Arcana);
+            Assert.AreEqual(4, actualNPCModel.Athletics);
+            Assert.AreEqual(5, actualNPCModel.Deception);
+            Assert.AreEqual(6, actualNPCModel.History);
+            Assert.AreEqual(7, actualNPCModel.Insight);
+            Assert.AreEqual(8, actualNPCModel.Intimidation);
+            Assert.AreEqual(9, actualNPCModel.Investigation);
+            Assert.AreEqual(10, actualNPCModel.Medicine);
+            Assert.AreEqual(11, actualNPCModel.Nature);
+            Assert.AreEqual(12, actualNPCModel.Perception);
+            Assert.AreEqual(13, actualNPCModel.Performance);
+            Assert.AreEqual(14, actualNPCModel.Persuasion);
+            Assert.AreEqual(15, actualNPCModel.Religion);
+            Assert.AreEqual(16, actualNPCModel.SleightOfHand);
+            Assert.AreEqual(17, actualNPCModel.Stealth);
+            Assert.AreEqual(18, actualNPCModel.Survival);
+        }
+
+        // Used to validate all Lists generated & presented in the Resistance tab
+        private void AssertLanguageModelList(ObservableCollection<LanguageModel> expected, ObservableCollection<LanguageModel> actual)
+        {
+            Assert.AreEqual(expected.Count, actual.Count);
+
+            // Sort both lists by ActionName so that the following loop will guarantee to work
+            List<LanguageModel> expectedLanguages = expected.ToList();
+            List<LanguageModel> actualLanguages = actual.ToList();
+
+            expectedLanguages.Sort((x, y) => x.Language.CompareTo(y.Language));
+            actualLanguages.Sort((x, y) => x.Language.CompareTo(y.Language));
+            for (int idx = 0; idx < expectedLanguages.Count; idx++)
+            {
+                Assert.AreEqual(expectedLanguages[idx].Language, actualLanguages[idx].Language);
+                Assert.AreEqual(expectedLanguages[idx].Selected, actualLanguages[idx].Selected);
+            }
+        }
+
+        [TestMethod()]
+        public void Skills_CommonLanguages()
+        {
+            ObservableCollection<LanguageModel> expectedLanguages = new LanguageController().GenerateStandardLanguages();
+
+            foreach (LanguageModel language in expectedLanguages)
+            {
+                switch (language.Language)
+                {
+                    case "Common":
+                    case "Elvish":
+                    case "Gnomish":
+                    case "Halfling":
+                    case "Thieves' Cant":
+                        language.Selected = true;
+                        break;
+                }
+            }
+
+            AssertLanguageModelList(expectedLanguages, LoadEngineerSuiteTestNPCFile().StandardLanguages);
+        }
+
+        [TestMethod()]
+        public void Skills_ExoticLanguages()
+        {
+            ObservableCollection<LanguageModel> expectedLanguages = new LanguageController().GenerateExoticLanguages();
+
+            foreach (LanguageModel language in expectedLanguages)
+            {
+                switch (language.Language)
+                {
+                    case "Celestial":
+                    case "Draconic":
+                    case "Infernal":
+                    case "Slyvan":
+                        language.Selected = true;
+                        break;
+                }
+            }
+
+            AssertLanguageModelList(expectedLanguages, LoadEngineerSuiteTestNPCFile().StandardLanguages);
+        }
+
+        [TestMethod()]
+        public void Skills_MonstrousLanguages()
+        {
+            ObservableCollection<LanguageModel> expectedLanguages = new LanguageController().GenerateMonsterLanguages();
+
+            foreach (LanguageModel language in expectedLanguages)
+            {
+                switch (language.Language)
+                {
+                    case "Aarakocra":
+                    case "Bullywug":
+                    case "Grell":
+                    case "Ice Toad":
+                    case "Modron":
+                    case "Slaad":
+                    case "Thri-kreen":
+                    case "Umber hulk":
+                        language.Selected = true;
+                        break;
+                }
+            }
+
+            AssertLanguageModelList(expectedLanguages, LoadEngineerSuiteTestNPCFile().StandardLanguages);
+        }
+
+        [TestMethod()]
+        public void Skills_Telepathy()
+        {
+            NPCModel actualNPCModel = LoadEngineerSuiteTestNPCFile();
+
+            Assert.AreEqual(true, actualNPCModel.Telepathy);
+            Assert.AreEqual(90, actualNPCModel.TelepathyRange);
+        }
+
+        #endregion
+
     }
 }
