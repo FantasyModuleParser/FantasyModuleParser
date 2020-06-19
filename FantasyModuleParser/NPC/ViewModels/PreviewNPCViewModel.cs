b7f36b995b3cb729681bd365444d6b700f93a6e3
EngineerSuite2 <62843245+EngineerSuite2@users.noreply.github.com>
2020-06-18 19:47:40 -0700

    Removed extra commas in Saving Throws, Senses, Skills. Challenge Rating now works in Preview Window.

--- FantasyModuleParser/NPC/ViewModels/PreviewNPCViewModel.cs
+++ FantasyModuleParser/NPC/ViewModels/PreviewNPCViewModel.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Forms;
 
 namespace FantasyModuleParser.NPC.ViewModels
@@ -25,28 +26,30 @@ namespace FantasyModuleParser.NPC.ViewModels
         public string CharismaAttribute { get; set; }
         public string SavingThrows { get; set; }
         public string Senses { get; set; }
+        public string Challenge { get; set; }
 
         public PreviewNPCViewModel()
         {
             npcController = new NPCController();
             NPCModel = npcController.GetNPCModel();
             SpeedDescription = UpdateSpeedDescription();
-            SkillsDescription = _updateSkillsDescription();
+            SkillsDescription = UpdateSkillsDescription();
             StrengthAttribute = UpdateStrengthAttribute();
             DexterityAttribute = UpdateDexterityAttribute();
             ConstitutionAttribute = UpdateConstitutionAttribute();
             IntelligenceAttribute = UpdateIntelligenceAttribute();
             WisdomAttribute = UpdateWisdomAttribute();
             CharismaAttribute = UpdateCharismaAttribute();
-            SavingThrows = _updateSavingThrows();
+            SavingThrows = UpdateSavingThrows();
             Senses = UpdateSenses();
+            Challenge = UpdateChallengeRating();
         }
 
         public PreviewNPCViewModel(NPCModel nPCModel)
         {
             NPCModel = nPCModel;
             SpeedDescription = UpdateSpeedDescription();
-            SkillsDescription = _updateSkillsDescription();    
+            SkillsDescription = UpdateSkillsDescription();    
         }
         #region UpdateAbilityScores
         public string UpdateStrengthAttribute()
@@ -142,25 +145,36 @@ namespace FantasyModuleParser.NPC.ViewModels
             if (NPCModel.Speed == 0)
                 stringBuilder.Append("0 ft., ");
             else 
-                stringBuilder.Append(_appendSpeedAttribute("", NPCModel.Speed, false));
-                stringBuilder.Append(_appendSpeedAttribute("climb", NPCModel.Climb, false));
-                stringBuilder.Append(_appendSpeedAttribute("fly", NPCModel.Fly, NPCModel.Hover));
-                stringBuilder.Append(_appendSpeedAttribute("burrow", NPCModel.Burrow, false));
-                stringBuilder.Append(_appendSpeedAttribute("swim", NPCModel.Swim, false));
+                stringBuilder.Append(AppendSpeedAttribute("", NPCModel.Speed, false));
+                stringBuilder.Append(AppendSpeedAttribute("climb", NPCModel.Climb, false));
+                stringBuilder.Append(AppendSpeedAttribute("fly", NPCModel.Fly, NPCModel.Hover));
+                stringBuilder.Append(AppendSpeedAttribute("burrow", NPCModel.Burrow, false));
+                stringBuilder.Append(AppendSpeedAttribute("swim", NPCModel.Swim, false));
 
-            stringBuilder.Remove(stringBuilder.Length - 2, 2);
+            if (stringBuilder.Length >= 2)
+                stringBuilder.Remove(stringBuilder.Length - 2, 2);
             return stringBuilder.ToString().Trim();
         }
 
-        private string _appendSpeedAttribute(string name, int value, bool hover)
+        private string AppendSpeedAttribute(string name, int value, bool hover)
         {
+            string delimiter = ", ";
             if (value != 0)
-                return name + " " + value + " ft." + (hover ? " (hover), ": ", ");
+                return name + " " + value + " ft." + (hover ? " (hover), ": ", ") + delimiter;
             return "";
         }
         #endregion
         #region UpdateSkills
-        private string _updateSkillsDescription()
+        public Visibility ShowSkills
+        {
+            get
+            {
+                if (SkillsDescription.Length > 0)
+                    return Visibility.Visible;
+                return Visibility.Collapsed;
+            }
+        }
+        private string UpdateSkillsDescription()
         {
             StringBuilder stringBuilder = new StringBuilder();
             stringBuilder.Append(appendSkill("Acrobatics", NPCModel.Acrobatics));
@@ -181,58 +195,80 @@ namespace FantasyModuleParser.NPC.ViewModels
             stringBuilder.Append(appendSkill("Sleight of Hand", NPCModel.SleightOfHand));
             stringBuilder.Append(appendSkill("Stealth", NPCModel.Stealth));
             stringBuilder.Append(appendSkill("Survival", NPCModel.Survival));
-        }
-            
+            if (stringBuilder.Length >= 2)
+                stringBuilder.Remove(stringBuilder.Length - 2, 2);
+            return stringBuilder.ToString().Trim();
+        } 
         private string appendSkill(string skillName, int skillValue)
         {
             string delimiter = ", ";
             if (skillValue != 0)
-                return skillName + ((skillValue < 0) ? " -" : " +") + skillValue + delimiter;
+                return skillName + ((skillValue < 0) ? " " : " +") + skillValue + delimiter;
             return "";
         }
         #endregion
         #region UpdateSavingThrows
-        private string _updateSavingThrows()
+        public Visibility ShowSavingThrows
+        {
+            get
+            {
+                if (SavingThrows.Length > 0)
+                    return Visibility.Visible;
+                return Visibility.Collapsed;
+            }
+        }
+        private string UpdateSavingThrows()
         {
             StringBuilder stringBuilder = new StringBuilder();
-            string delimiter = ", ";
-            stringBuilder.Append(appendSavingThrow("Str", NPCModel.SavingThrowStr)).Append(delimiter);
-            stringBuilder.Append(appendSavingThrow("Dex", NPCModel.SavingThrowDex)).Append(delimiter);
-            stringBuilder.Append(appendSavingThrow("Con", NPCModel.SavingThrowCon)).Append(delimiter);
-            stringBuilder.Append(appendSavingThrow("Int", NPCModel.SavingThrowInt)).Append(delimiter);
-            stringBuilder.Append(appendSavingThrow("Wis", NPCModel.SavingThrowWis)).Append(delimiter);
-            stringBuilder.Append(appendSavingThrow("Cha", NPCModel.SavingThrowCha)).Append(delimiter);
-            stringBuilder.Remove(stringBuilder.Length - 2, 2);
+            stringBuilder.Append(appendSavingThrow("Str", NPCModel.SavingThrowStr));
+            stringBuilder.Append(appendSavingThrow("Dex", NPCModel.SavingThrowDex));
+            stringBuilder.Append(appendSavingThrow("Con", NPCModel.SavingThrowCon));
+            stringBuilder.Append(appendSavingThrow("Int", NPCModel.SavingThrowInt));
+            stringBuilder.Append(appendSavingThrow("Wis", NPCModel.SavingThrowWis));
+            stringBuilder.Append(appendSavingThrow("Cha", NPCModel.SavingThrowCha));
+            if (stringBuilder.Length >= 2)
+            {
+                stringBuilder.Remove(stringBuilder.Length - 2, 2);
+            }
             return stringBuilder.ToString();
         }
         private string appendSavingThrow(string savingThrowName, int savingThrowValue)
         {
+            string delimiter = ", ";
             if (savingThrowValue != 0)
-                return savingThrowName + ((savingThrowValue < 0) ? " " : " +") + savingThrowValue;
+            {
+                return savingThrowName + ((savingThrowValue < 0) ? " " : " +") + savingThrowValue + delimiter;
+            }
             return "";
         }
         #endregion
-
         #region UpdateSenses
         private string UpdateSenses()
         {
             StringBuilder stringBuilder = new StringBuilder();
-            string delimiter = ", ";
-            stringBuilder.Append(appendSenses("darkvision ", NPCModel.Darkvision, " ft.")).Append(delimiter);
-            stringBuilder.Append(appendSenses("blindsight ", NPCModel.Blindsight, " ft.")).Append(delimiter);
-            stringBuilder.Append(appendSenses("tremorsense ", NPCModel.Tremorsense, " ft.")).Append(delimiter);
-            stringBuilder.Append(appendSenses("truesight ", NPCModel.Truesight, " ft.")).Append(delimiter);
-            stringBuilder.Append(appendSenses("passive perception ", NPCModel.PassivePerception, " ft.")).Append(delimiter);
-
-            // Remove the last comma
-            stringBuilder.Remove(stringBuilder.Length - 2, 2);
+            stringBuilder.Append(appendSenses("darkvision ", NPCModel.Darkvision, " ft."));
+            stringBuilder.Append(appendSenses("blindsight ", NPCModel.Blindsight, " ft."));
+            stringBuilder.Append(appendSenses("tremorsense ", NPCModel.Tremorsense, " ft."));
+            stringBuilder.Append(appendSenses("truesight ", NPCModel.Truesight, " ft."));
+            stringBuilder.Append(appendSenses("passive perception ", NPCModel.PassivePerception, " ft."));
+            if (stringBuilder.Length >= 2)
+                stringBuilder.Remove(stringBuilder.Length - 2, 2);
             return stringBuilder.ToString();
         }
         private string appendSenses(string senseName, int senseValue, string senseRange)
         {
+            string delimiter = ", ";
             if (senseValue != 0)
-                return senseName + senseValue + senseRange;
+                return senseName + senseValue + senseRange + delimiter;
             return "";
+        }
+        #endregion
+        #region UpdateChallengeRating
+        private string UpdateChallengeRating()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(NPCModel.ChallengeRating + " (" + NPCModel.XP + ") XP");
+            return stringBuilder.ToString();
         }
         #endregion
     }
