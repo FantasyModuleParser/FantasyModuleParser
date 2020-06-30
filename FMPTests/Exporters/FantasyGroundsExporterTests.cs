7da38ffc1ccdb3cc54df37c3b40d4a35e598ba60
Fskallos <fredska@gmail.com>
2020-06-30 00:27:41 -0400

    Beginning WIP on FG Exporter

---
+++ FMPTests/Exporters/FantasyGroundsExporterTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FantasyModuleParser.Exporters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FantasyModuleParser.NPC.Models;
+using FantasyModuleParser.Main.Services;
+
+namespace FantasyModuleParser.Exporters.Tests
+{
+    [TestClass()]
+    public class FantasyGroundsExporterTests
+    {
+        private ModuleModel moduleModel;
+        
+        private FantasyGroundsExporter exporter;
+        [TestInitialize]
+        public void Initialize()
+        {
+            moduleModel = new ModuleModel();
+            moduleModel.Name = "Test Module";
+            moduleModel.Author = "Fredska";
+            moduleModel.Category = "Supplement";
+            moduleModel.ModulePath = "Path\\To\\Everything";
+
+            exporter = new FantasyGroundsExporter();
+        }
+
+        [TestMethod()]
+        public void GenerateDefinitionXmlContentTest()
+        {
+            string xmlContent = exporter.GenerateDefinitionXmlContent(moduleModel);
+
+            string expected = "<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n" +
+                "<root version=\"3.3\">\r\n" +
+                "  <name>Test Module</name>\r\n" +
+                "  <category>Supplement</category>\r\n" +
+                "  <author>Fredska</author>\r\n" +
+                "  <ruleset>5E</ruleset>\r\n" +
+                "</root>";
+            Assert.AreEqual(expected, xmlContent);
+        }
+    }
+}
---
+++ FantasyModuleParser/Exporters/FantasyGroundsExporter.cs
@@ -0,0 +1,87 @@
+﻿using FantasyModuleParser.NPC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace FantasyModuleParser.Exporters
+{
+    public class FantasyGroundsExporter : IExporter
+    {
+        public void CreateModule(ModuleModel moduleModel)
+        {
+            if (moduleModel.ModulePath == null || moduleModel.ModulePath.Length == 0)
+            {
+                throw new ApplicationException("No Module Path has been set");
+            }
+
+            // A blank module consists of two files;  db.xml & definition.xml
+        }
+
+        private void GenerateDBXmlFile(ModuleModel moduleModel)
+        {
+
+        }
+
+        public string GenerateDefinitionXmlContent(ModuleModel moduleModel)
+        {
+            XmlDocument xml = new XmlDocument();
+
+            XmlElement rootElement = xml.CreateElement("root");
+            rootElement.SetAttribute("version", "3.3"); // Not sure how relevant this is
+
+            xml.AppendChild(rootElement);
+
+            XmlElement nameElement = xml.CreateElement("name");
+            nameElement.InnerText = moduleModel.Name;
+
+            XmlElement categoryElement = xml.CreateElement("category");
+            categoryElement.InnerText = moduleModel.Category;
+
+            XmlElement authorElement = xml.CreateElement("author");
+            authorElement.InnerText = moduleModel.Author;
+
+            XmlElement ruleSetElement = xml.CreateElement("ruleset");
+            ruleSetElement.InnerText = "5E";
+
+            rootElement.AppendChild(nameElement);
+            rootElement.AppendChild(categoryElement);
+            rootElement.AppendChild(authorElement);
+            rootElement.AppendChild(ruleSetElement);
+
+            xml.AppendChild(rootElement);
+
+            return writeXmlDocumentToString(xml);
+        }
+
+        private string writeXmlDocumentToString(XmlDocument xmlDocument)
+        {
+            XmlWriterSettings settings = new XmlWriterSettings
+            {
+                Encoding = Encoding.UTF8,
+                ConformanceLevel = ConformanceLevel.Document,
+                OmitXmlDeclaration = false,
+                CloseOutput = true,
+                Indent = true,
+                IndentChars = "  ",
+                NewLineHandling = NewLineHandling.Replace
+            };
+
+            string document = "";
+            using (StringWriter sw = new StringWriter())
+            using (XmlWriter writer = XmlWriter.Create(sw, settings))
+            {
+                xmlDocument.WriteContentTo(writer);
+                writer.Close();
+                document = sw.ToString();
+            }
+
+            return document;
+        }
+    }
+}
