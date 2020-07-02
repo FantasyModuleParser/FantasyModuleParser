402c49241f7979a5136c27a0f4355f09e78e656d
Fskallos <fredska@gmail.com>
2020-07-01 23:25:19 -0400

    Working FGU exporter - Exports a blank module, but loads into FGU without issue

--- FantasyModuleParser/Exporters/FantasyGroundsExporter.cs
+++ FantasyModuleParser/Exporters/FantasyGroundsExporter.cs
@@ -2,7 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Globalization;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,11 +22,62 @@ namespace FantasyModuleParser.Exporters
                 throw new ApplicationException("No Module Path has been set");
             }
 
+            if (moduleModel.Name == null || moduleModel.Name.Length == 0)
+            {
+                throw new ApplicationException("No Module Name has been set");
+            }
+
+            string moduleFolderPath = Path.Combine(moduleModel.ModulePath, moduleModel.Name);
+
+            // Create the folder all content will go into based on the Module name
+            System.IO.Directory.CreateDirectory(moduleFolderPath);
+
             // A blank module consists of two files;  db.xml & definition.xml
+            string dbXmlFileContent = GenerateDBXmlFile(moduleModel);
+            string definitionXmlFileContent = GenerateDefinitionXmlContent(moduleModel);
+
+            // Write the string array to a new file named "WriteLines.txt".
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(moduleFolderPath, "db.xml")))
+            {
+                outputFile.WriteLine(dbXmlFileContent);
+            }
+
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(moduleFolderPath, "definition.xml")))
+            {
+                outputFile.WriteLine(definitionXmlFileContent);
+            }
+
+            //TODO:  Write out any other file content below!
+
+            // ============================================
+
+            // Zipping up the folder contents and naming to *.mod
+
+            // First need to check if the file exists;  If so, delete it
+            if (File.Exists(@Path.Combine(moduleModel.ModulePath, moduleModel.Name + ".mod")))
+            {
+                File.Delete(@Path.Combine(moduleModel.ModulePath, moduleModel.Name + ".mod"));
+            }
+            ZipFile.CreateFromDirectory(moduleFolderPath, Path.Combine(moduleModel.ModulePath, moduleModel.Name + ".mod"));
         }
 
-        private void GenerateDBXmlFile(ModuleModel moduleModel)
+        public string GenerateDBXmlFile(ModuleModel moduleModel)
         {
+            //Blank Mod:
+            /*
+             *  <?xml version="1.0" encoding="utf-8"?>
+             *  <root version="4" dataversion="20200528" release="8|CoreRPG:4" />
+             */
+
+            XmlDocument xml = new XmlDocument();
+
+            XmlElement rootElement = xml.CreateElement("root");
+            rootElement.SetAttribute("version", "4"); // Not sure how relevant this is
+            rootElement.SetAttribute("dataversion", DateTime.Now.ToString("yyyyMMdd", CultureInfo.InvariantCulture));
+            rootElement.SetAttribute("release", "8|CoreRPG:4");
+            xml.AppendChild(rootElement);
+
+            return writeXmlDocumentToString(xml);
 
         }
 
@@ -33,7 +86,7 @@ namespace FantasyModuleParser.Exporters
             XmlDocument xml = new XmlDocument();
 
             XmlElement rootElement = xml.CreateElement("root");
-            rootElement.SetAttribute("version", "3.3"); // Not sure how relevant this is
+            rootElement.SetAttribute("version", "4"); // Not sure how relevant this is
 
             xml.AppendChild(rootElement);
 
@@ -73,7 +126,7 @@ namespace FantasyModuleParser.Exporters
             };
 
             string document = "";
-            using (StringWriter sw = new StringWriter())
+            using (StringWriter sw = new StringWriterWithEncoding())
             using (XmlWriter writer = XmlWriter.Create(sw, settings))
             {
                 xmlDocument.WriteContentTo(writer);
@@ -82,6 +135,23 @@ namespace FantasyModuleParser.Exporters
             }
 
             return document;
+        }
+
+        public sealed class StringWriterWithEncoding : StringWriter
+        {
+            private readonly Encoding encoding;
+
+            public StringWriterWithEncoding() { }
+
+            public StringWriterWithEncoding(Encoding encoding)
+            {
+                this.encoding = encoding;
+            }
+
+            public override Encoding Encoding
+            {
+                get { return encoding; }
+            }
         }
     }
 }
