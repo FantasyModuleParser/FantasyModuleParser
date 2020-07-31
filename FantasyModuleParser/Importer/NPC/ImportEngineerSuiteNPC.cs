d8d0b009e5f8a14fff9f53875f6e6b3f0e524282
fredska <fredska@gmail.com>
2020-07-30 23:37:04 -0400

    Bugfix found where standard Action tried to process an empty string.

--- FantasyModuleParser/Importer/NPC/ImportEngineerSuiteNPC.cs
+++ FantasyModuleParser/Importer/NPC/ImportEngineerSuiteNPC.cs
@@ -899,6 +899,10 @@ namespace FantasyModuleParser.Importer.NPC
         /// </summary>
         public void ParseStandardAction(NPCModel npcModel, string standardAction)
         {
+            // Don't deal w/ an empty string.
+            if (standardAction.Length == 0 || standardAction.Trim().Length == 0)
+                return;
+
             string[] standardActionArray;
             StringBuilder stringBuilder = new StringBuilder();
             if (standardAction.StartsWith(Multiattack.LocalActionName))
