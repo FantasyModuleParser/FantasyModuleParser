c85615dc701de59dfd2e9bf3793e2a8d05eb6a98
fredska <fredska@gmail.com>
2020-08-05 21:06:42 -0400

    Adding an NPC now updates the NPC dropdown selection list to include the newly added NPC

--- FantasyModuleParser/NPC/UserControls/Options/NPCOptionControl.xaml.cs
+++ FantasyModuleParser/NPC/UserControls/Options/NPCOptionControl.xaml.cs
@@ -165,8 +165,9 @@ namespace FantasyModuleParser.NPC.UserControls.Options
 			}
 
 			ModuleService moduleService = new ModuleService();
-			try { 
+			try {
 				moduleService.AddNPCToCategory(npcController.GetNPCModel(), (FGCategoryComboBox.SelectedItem as CategoryModel).Name);
+				Refresh();
 				MessageBox.Show("NPC has been added to the project");
 			}
 			catch (Exception exception)
