f80488aeab699a9a573713b10ca383873df82205
fredska <fredska@gmail.com>
2020-08-14 00:04:44 -0400

    DescriptionUC - Markdown Converter WIP with unit tests & Integration tests

---
+++ FMPTests/NPC/ViewModels/DescriptionUCViewModelTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace FantasyModuleParser.NPC.ViewModels.Tests
+{
+    [TestClass()]
+    public class DescriptionUCViewModelTests
+    {
+        private DescriptionUCViewModel viewModel;
+        [TestInitialize]
+        public void Initialize()
+        {
+            viewModel = new DescriptionUCViewModel();
+        }
+
+        [TestMethod()]
+        [DataRow("#Description", "<h>Description</h>")]
+        [DataRow("**Bold**", "<b>Bold</b>")]
+        [DataRow("*Italized*", "<i>Italized</i>")]
+        public void ValidateXMLTest(string actualContent, string expectedContent)
+        {
+            string transformedActualContent = viewModel.ValidateXML(actualContent);
+
+            Assert.AreEqual(expectedContent, transformedActualContent);
+        }
+
+        [TestMethod()]
+        // FMPTests.Resources.V1_npc_all.npc
+        [DataRow("FMPTests.Resources.MarkdownDescriptions.AncestralBarbarianTest.md", "FMPTests.Resources.MarkdownDescriptions.ExpectedResult.AncestralBarbarianExpected.txt")]
+        public void Validate_DescriptionXMLTransform_File(string actualFileDataPath, string expectedFileDataPath)
+        {
+            string actualDataContent = GetEmbeddedResourceFileContent(actualFileDataPath);
+            string expectedDataContent = GetEmbeddedResourceFileContent(expectedFileDataPath);
+
+            string transformedActualContent = viewModel.ValidateXML(actualDataContent);
+
+            Assert.AreEqual(expectedDataContent, transformedActualContent);
+        }
+
+        private string GetEmbeddedResourceFileContent(string embeddedResourcePath)
+        {
+
+            using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(@embeddedResourcePath))
+            using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
--- FantasyModuleParser/NPC/ViewModels/DescriptionUCViewModel.cs
+++ FantasyModuleParser/NPC/ViewModels/DescriptionUCViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using FantasyModuleParser.NPC.Controllers;
 using FantasyModuleParser.NPC.ViewModel;
+using Microsoft.Toolkit.Parsers.Markdown;
+using Microsoft.Toolkit.Parsers.Markdown.Blocks;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +12,6 @@ namespace FantasyModuleParser.NPC.ViewModels
 {
     public class DescriptionUCViewModel : ViewModelBase
     {
-        private string _description;
         private NPCModel _npcModel;
         public string Description { 
             get 
@@ -27,10 +28,41 @@ namespace FantasyModuleParser.NPC.ViewModels
             npcController = new NPCController();
             _npcModel = npcController.GetNPCModel();
         }
+        /// <summary>
+        /// Specifically used for Unit test integration of 
+        /// </summary>
+        /// <param name="npcModel"></param>
+        public DescriptionUCViewModel(NPCModel npcModel)
+        {
+            this._npcModel = npcModel;
+        }
 
         public void ValidateXML()
         {
+            ValidateXML(Description);
+        }
+        public String ValidateXML(String inputMarkdownText)
+        {
+            MarkdownDocument markdownDocument = new MarkdownDocument();
+            markdownDocument.Parse(inputMarkdownText);
 
+            StringBuilder output = new StringBuilder();
+            foreach (var element in markdownDocument.Blocks)
+            {
+                if (element is HeaderBlock header)
+                {
+                    //Console.WriteLine($"Header: {header.ToString()}");
+                    if (header.HeaderLevel == 1)
+                        output.Append("<h>").Append(header.ToString()).Append("</h>");
+                }
+
+                if (element is ParagraphBlock paragraph)
+                {
+                    Console.WriteLine($"Paragraph: {paragraph.ToString()}");
+                }
+            }
+
+            return output.ToString();
         }
         public void Refresh()
         {
