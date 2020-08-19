69aafb01ecdd4b67fbd64abf9a48963ad44ffc2e
EngineerSuite2 <62843245+EngineerSuite2@users.noreply.github.com>
2020-08-18 18:13:14 -0700

    Updated version to 0.1.4.1a

--- FantasyModuleParser/Main/Views/About.xaml.cs
+++ FantasyModuleParser/Main/Views/About.xaml.cs
@@ -26,7 +26,7 @@ namespace FantasyModuleParser.Main
             InitializeComponent();
             // Enable it so the popup window can close on the Escape key
             PreviewKeyDown += (sender, eventArgs) => { if (eventArgs.Key == Key.Escape) Close(); };
-            versionlabel.Content = "v" + System.Windows.Forms.Application.ProductVersion + "-Alpha";
+            versionlabel.Content = "v" + System.Windows.Forms.Application.ProductVersion + "a";
         }
         private void ESExit_Click(object sender, RoutedEventArgs e)
         {
