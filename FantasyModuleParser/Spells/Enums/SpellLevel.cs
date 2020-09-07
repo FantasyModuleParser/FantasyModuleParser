b7f00535f346e58073b9c67c34db2a0580ab95e7
EngineerSuite2 <62843245+EngineerSuite2@users.noreply.github.com>
2020-09-07 15:05:39 -0700

    Added Spell Option main window WIP Added Spell Level enumerator

---
+++ FantasyModuleParser/Spells/Enums/SpellLevel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+
+namespace FantasyModuleParser.Spells.Enums
+{
+    public enum SpellLevel
+    {
+		[Description("cantrip")]
+		Cantrip = 0,
+
+		[Description("1st level")]
+		FirstLevel = 1,
+
+		[Description("2nd level")]
+		SecondLevel = 2,
+
+		[Description("3rd level")]
+		ThirdLevel = 3,
+
+		[Description("4th level")]
+		FourthLevel = 4,
+
+		[Description("5th level")]
+		FifthLevel = 5,
+
+		[Description("6th level")]
+		SixthLevel = 6,
+
+		[Description("7th level")]
+		SeventhLevel = 7,
+
+		[Description("8th level")]
+		EighthLevel = 8,
+
+		[Description("9th level")]
+		NinthLevel = 9,
+	}
+}
---
+++ FantasyModuleParser/Spells/Spell_Option.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FantasyModuleParser.Spells
+{
+    /// <summary>
+    /// Interaction logic for Spell_Option.xaml
+    /// </summary>
+    public partial class Spell_Option : Window
+    {
+        public Spell_Option()
+        {
+            InitializeComponent();
+        }
+    }
+}
