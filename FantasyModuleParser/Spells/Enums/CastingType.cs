214ba170431fb3ab96146d3dda9eb2d5aae2e1a0
EngineerSuite2 <62843245+EngineerSuite2@users.noreply.github.com>
2020-09-08 08:40:52 -0700

    Spell User Control WIP Added all enumerators

---
+++ FantasyModuleParser/Spells/Enums/CastingType.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace FantasyModuleParser.Spells.Enums
+{
+    public enum CastingType
+    {
+		[Description("--")]
+		None = 0,
+
+		[Description("action")]
+		Action = 1,
+
+		[Description("bonus action")]
+		BonusAction = 2,
+
+		[Description("reaction")]
+		Reaction = 3,
+
+		[Description("hour")]
+		Hour = 4,
+
+		[Description("minute")]
+		Minute = 5,
+	}
+}
---
+++ FantasyModuleParser/Spells/Enums/DurationUnit.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace FantasyModuleParser.Spells.Enums
+{
+    public enum DurationUnit
+    {
+		[Description("--")]
+		None = 0,
+
+		[Description("round")]
+		Round = 1,
+
+		[Description("minute")]
+		Minute = 2,
+
+		[Description("hour")]
+		Hour = 3,
+
+		[Description("day")]
+		Day = 4,
+	}
+}
