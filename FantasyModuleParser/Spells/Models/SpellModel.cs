8669f8705ffcba21f790df3887b9e503ab4c4e98
fredska <fredska@gmail.com>
2020-09-29 17:04:58 -0400

    Feature: WIP on backend framework for Spells

---
+++ FantasyModuleParser/Spells/Models/SpellModel.cs
@@ -0,0 +1,24 @@
+﻿using FantasyModuleParser.Spells.Enums;
+
+namespace FantasyModuleParser.Spells.Models
+{
+    public class SpellModel
+    {
+        public string SpellName { get; set; }
+        public SpellLevel SpellLevel { get; set; }
+        public SpellSchool SpellSchool { get; set; }
+        public bool IsRitual { get; set; }
+        public string CastingTime { get; set; }
+        public CastingType CastingType { get; set; }
+        public string Range { get; set; }
+        public RangeType RangeType { get; set; }
+        public bool IsVerbalComponent { get; set; }
+        public bool IsSomaticComponent { get; set; }
+        public bool IsMaterialComponent { get; set; }
+        public int DurationTime { get; set; }
+        public DurationType DurationType { get; set; }
+        public DurationUnit DurationUnit { get; set; }
+        public string CastBy { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ FantasyModuleParser/Spells/ViewModels/SpellViewModel.cs
@@ -0,0 +1,57 @@
+﻿using FantasyModuleParser.NPC.ViewModel;
+using FantasyModuleParser.Spells.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FantasyModuleParser.Spells.ViewModels
+{
+    public class SpellViewModel : ViewModelBase
+    {
+        // TODO: Include SettingsModel & Service when finished
+
+        private SpellModel _spellModel;
+        public SpellModel SpellModel
+        {
+            get
+            {
+                return this._spellModel;
+            }
+            set
+            {
+                this._spellModel = value;
+                RaisePropertyChanged(nameof(SpellModel));
+            }
+        }
+        public SpellViewModel()
+        {
+            SpellModel = new SpellModel();
+        }
+
+        public void Save()
+        {
+            if (!string.IsNullOrWhiteSpace(SpellModel.SpellName)) { 
+                using (StreamWriter file = File.CreateText(@"C:\Users\darkp\AppData\Roaming\FMP\Spells\" + SpellModel.SpellName + ".spl"))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    serializer.Formatting = Formatting.Indented;
+                    serializer.Serialize(file, SpellModel);
+                }
+            }
+        }
+
+        public void Save(string filePath)
+        {
+            using (StreamWriter file = File.CreateText(@filePath))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Formatting = Formatting.Indented;
+                serializer.Serialize(file, SpellModel);
+            }
+        }
+    }
+}
