441243f6e08430d37f4b2fb4f65acf3c4cb7c303
tstory30 <30578888+tstory30@users.noreply.github.com>
2020-10-29 09:25:46 -0700

    Added Spell Exporter coding to New-Options-Spell

--- FantasyModuleParser/Spells/SpellOptionControl.xaml.cs
+++ FantasyModuleParser/Spells/SpellOptionControl.xaml.cs
@@ -18,22 +18,19 @@ namespace FantasyModuleParser.Spells
         {
             InitializeComponent();
             SpellViewModel spellViewModel = DataContext as SpellViewModel;
-
             ReactionDescription.IsEnabled = spellViewModel.SpellModel.CastingType == Enums.CastingType.Reaction;
             DurationTime.IsEnabled = spellViewModel.SpellModel.DurationType == Enums.DurationType.Time;
             DurationUnit.IsEnabled = spellViewModel.SpellModel.DurationType == Enums.DurationType.Time;
         }
-
         private void SaveSpellButton_Click(object sender, System.Windows.RoutedEventArgs e)
         {
             (DataContext as SpellViewModel).Save();
         }
-
         private void DurationSelectionEnabled_ComboboxChanged(object sender, SelectionChangedEventArgs e)
         {
-            if(!IsSpellModelNull())
-            { 
-                if(DurationTime != null)
+            if (!IsSpellModelNull())
+            {
+                if (DurationTime != null)
                     DurationTime.IsEnabled = (DataContext as SpellViewModel).SpellModel.DurationType == Enums.DurationType.Time;
                 if (DurationUnit != null)
                     DurationUnit.IsEnabled = (DataContext as SpellViewModel).SpellModel.DurationType == Enums.DurationType.Time;
@@ -43,8 +40,10 @@ namespace FantasyModuleParser.Spells
         private void Casting_Changed(object sender, EventArgs e)
         {
             SpellModel spellModel = (DataContext as SpellViewModel).SpellModel;
+            if (spellModel == null)
+                return;
             if (ReactionDescription != null && !IsSpellModelNull())
-            { 
+            {
                 ReactionDescription.IsEnabled = spellModel.CastingType == Enums.CastingType.Reaction;
                 CastingDescriptionLabel.IsEnabled = spellModel.CastingType == Enums.CastingType.Reaction;
             }
@@ -61,52 +60,46 @@ namespace FantasyModuleParser.Spells
                     CastingTimeTB.IsEnabled = true;
                     break;
             }
-
             if (spellModel.CastingType != Enums.CastingType.None)
                 CastingDisplayValue.Text = spellModel.CastingTime + " "
                 + spellModel.CastingType.GetDescription()
                 + (spellModel.CastingTime > 1 ? "s" : "")
-                + (String.IsNullOrWhiteSpace(spellModel.ReactionDescription)? "" : ", " + spellModel.ReactionDescription);
+                + (String.IsNullOrWhiteSpace(spellModel.ReactionDescription) ? "" : ", " + spellModel.ReactionDescription);
             else
                 CastingDisplayValue.Text = "";
-
             //if(OnViewStatUpdate != null)
             //    OnViewStatUpdate.Invoke(this, EventArgs.Empty);
         }
-
         private bool IsSpellModelNull() { return (DataContext as SpellViewModel).SpellModel == null; }
-
         private void ViewStatBlock_Click(object sender, System.Windows.RoutedEventArgs e)
         {
             OnViewStatBlock.Invoke(this, EventArgs.Empty);
         }
-
         private void SpellOptionControl_Loaded(object sender, System.Windows.RoutedEventArgs e)
         {
             (DataContext as SpellViewModel).Refresh();
         }
-
         private void CategoryCB_SelectionChange(object sender, SelectionChangedEventArgs e)
         {
-
+            ComboBox comboBox = (sender as ComboBox);
+            (DataContext as SpellViewModel).SelectedCategoryModel = comboBox.SelectedItem as CategoryModel;
         }
-
         private void LoadSpellButton_Click(object sender, System.Windows.RoutedEventArgs e)
         {
             (DataContext as SpellViewModel).LoadSpell();
         }
-
         private void AddToProjectButton_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            if(FGCategoryComboBox == null || FGCategoryComboBox.SelectedItem == null)
+            if (FGCategoryComboBox == null || FGCategoryComboBox.SelectedItem == null)
                 (DataContext as SpellViewModel).AddSpellToModule(null);
             else
                 (DataContext as SpellViewModel).AddSpellToModule((FGCategoryComboBox.SelectedItem as CategoryModel).Name);
         }
-
         private void RangeComboBox_SelectionChanged(object sender, EventArgs e)
         {
             SpellModel spellModel = (DataContext as SpellViewModel).SpellModel;
+            if (spellModel == null)
+                return;
             if (RangeValueTB == null)
                 return;
 
@@ -114,10 +107,58 @@ namespace FantasyModuleParser.Spells
                 RangeDisplayValue.Text = spellModel.Range + " feet";
             else
                 RangeDisplayValue.Text = spellModel.RangeType == Enums.RangeType.None ? "" : spellModel.RangeType.GetDescription();
-
             RangeDistanceLabel.IsEnabled = RangeValueTB.IsEnabled;
         }
+        private void SpellComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = (sender as ComboBox);
+            (DataContext as SpellViewModel).SpellModel = comboBox.SelectedItem as SpellModel;
+        }
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            SpellViewModel spellViewModel = DataContext as SpellViewModel;
+            CategoryModel categoryModel = FGCategoryComboBox.SelectedItem as CategoryModel;
 
+            // Ignore any button actions if no Spells exist for the selected Category
+            if (categoryModel.SpellModels.Count <= 0)
+                return;
+
+            int categoryIndex = SpellComboBox.SelectedIndex;
+            switch ((sender as Button).Name)
+            {
+                case nameof(PrevSpellBtn):
+
+                    if (categoryModel != null)
+                    {
+                        if (categoryIndex <= 0)
+                            categoryIndex = categoryModel.SpellModels.Count - 1;
+                        else
+                            categoryIndex--;
+
+                        // By updating the selected Item here, it will invoke CategorySelectedNPCComboBox_SelectionChanged event 
+                        // because the CategorySelectedNPCComboBox selected item has changed
+                        SpellComboBox.SelectedItem = categoryModel.SpellModels[categoryIndex];
+                    }
+                    break;
+                case nameof(NextSpellBtn):
+                    if (categoryModel != null)
+                    {
+                        if (categoryIndex == categoryModel.SpellModels.Count - 1)
+                            categoryIndex = 0;
+                        else
+                            categoryIndex++;
+
+                        // By updating the selected Item here, it will invoke CategorySelectedNPCComboBox_SelectionChanged event 
+                        // because the CategorySelectedNPCComboBox selected item has changed
+                        SpellComboBox.SelectedItem = categoryModel.SpellModels[categoryIndex];
+                    }
+                    break;
+            }
+        }
+        private void NewSpellButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            (DataContext as SpellViewModel).SpellModel = new SpellModel();
+        }
         private void SelectCasters_Click(object sender, System.Windows.RoutedEventArgs e)
         {
             new CastByWindow().ShowDialog();
--- FantasyModuleParser/Spells/ViewModels/SpellViewModel.cs
+++ FantasyModuleParser/Spells/ViewModels/SpellViewModel.cs
@@ -7,7 +7,6 @@ using Newtonsoft.Json;
 using System;
 using System.IO;
 using System.Windows;
-
 namespace FantasyModuleParser.Spells.ViewModels
 {
     public class SpellViewModel : ViewModelBase
@@ -18,6 +17,7 @@ namespace FantasyModuleParser.Spells.ViewModels
         private SpellModel _spellModel;
         private ModuleService _moduleService;
         private ModuleModel _moduleModel;
+        private CategoryModel _selectedCategoryModel;
         private ISpellService _spellService;
         public SpellModel SpellModel
         {
@@ -31,7 +31,6 @@ namespace FantasyModuleParser.Spells.ViewModels
                 RaisePropertyChanged(nameof(SpellModel));
             }
         }
-
         public ModuleModel ModuleModel
         {
             get
@@ -44,22 +43,32 @@ namespace FantasyModuleParser.Spells.ViewModels
                 RaisePropertyChanged(nameof(ModuleModel));
             }
         }
+        public CategoryModel SelectedCategoryModel
+        {
+            get
+            {
+                return this._selectedCategoryModel;
+            }
+            set
+            {
+                this._selectedCategoryModel = value;
+                RaisePropertyChanged(nameof(SelectedCategoryModel));
+            }
+        }
         public SpellViewModel()
         {
             SpellModel = new SpellModel();
             _settingsService = new SettingsService();
             _settingsModel = _settingsService.Load();
-
             _moduleService = new ModuleService();
             ModuleModel = _moduleService.GetModuleModel();
             _spellService = new SpellService();
         }
-
         public void Save()
         {
             Directory.CreateDirectory(_settingsModel.SpellFolderLocation);
-
-            if (!string.IsNullOrWhiteSpace(SpellModel.SpellName)) { 
+            if (!string.IsNullOrWhiteSpace(SpellModel.SpellName))
+            {
                 using (StreamWriter file = File.CreateText(_settingsModel.SpellFolderLocation + @"\" + SpellModel.SpellName + ".spl"))
                 {
                     JsonSerializer serializer = new JsonSerializer();
@@ -68,7 +77,6 @@ namespace FantasyModuleParser.Spells.ViewModels
                 }
             }
         }
-
         public void Save(string filePath)
         {
             using (StreamWriter file = File.CreateText(@filePath))
@@ -82,21 +90,22 @@ namespace FantasyModuleParser.Spells.ViewModels
         public void LoadSpell()
         {
             SpellModel = _spellService.Load(_settingsModel.SpellFolderLocation);
+            SpellModel loadedSpellModel = _spellService.Load(_settingsModel.SpellFolderLocation);
+            if (loadedSpellModel != null)
+                SpellModel = loadedSpellModel;
         }
 
         public void Refresh()
         {
             ModuleModel = _moduleService.GetModuleModel();
         }
-
         public void AddSpellToModule(string categoryValue)
         {
-            if(ModuleModel == null || ModuleModel.Categories == null || ModuleModel.Categories.Count == 0 || categoryValue == null)
+            if (ModuleModel == null || ModuleModel.Categories == null || ModuleModel.Categories.Count == 0 || categoryValue == null)
             {
                 MessageBox.Show("No Module Project loaded!\nPlease create / load a Module through Options -> Manage Project");
                 return;
             }
-
             try
             {
                 _moduleService.AddSpellToCategory(SpellModel, categoryValue);
@@ -104,7 +113,7 @@ namespace FantasyModuleParser.Spells.ViewModels
             }
             catch (Exception exception)
             {
-                MessageBox.Show("Error detected while adding NPC to button :: " + exception.Message);
+                MessageBox.Show("Error detected while adding Spell to Project :: " + exception.Message);
             }
         }
     }
