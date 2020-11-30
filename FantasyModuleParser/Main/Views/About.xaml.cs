f740193a482172f9e947ad8916b5d6fca4d9fa03
tstory30 <30578888+tstory30@users.noreply.github.com>
2020-11-30 09:15:42 -0700

    Updated About window as this is a hotfix

--- FantasyModuleParser/Main/Views/About.xaml.cs
+++ FantasyModuleParser/Main/Views/About.xaml.cs
@@ -1,18 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Forms;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace FantasyModuleParser.Main
 {
@@ -26,7 +13,7 @@ namespace FantasyModuleParser.Main
             InitializeComponent();
             // Enable it so the popup window can close on the Escape key
             PreviewKeyDown += (sender, eventArgs) => { if (eventArgs.Key == Key.Escape) Close(); };
-            versionlabel.Content = "v" + System.Windows.Forms.Application.ProductVersion;
+            versionlabel.Content = "v" + System.Windows.Forms.Application.ProductVersion + "a";
         }
         private void ESExit_Click(object sender, RoutedEventArgs e)
         {
