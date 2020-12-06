c04434456abb7af5ed947c6295c1a9cebe572e8b
fredska <fredska@gmail.com>
2020-12-05 23:17:51 -0500

    Added integration tests for Spell importer

--- FMPTests/Importer/Spells/ImportPDFSpellFileTests.cs
+++ FMPTests/Importer/Spells/ImportPDFSpellFileTests.cs
@@ -58,6 +58,55 @@ namespace FMPTests_Spells.Importer
 
             //TODO:  Assert the Description
         }
+        [TestMethod]
+        public void ColorSpray()
+        {
+            SpellModel expectedSpellModel = LoadSpellFileContent(HotDQ_BASE_PATH + "ColorSpray.txt");
+
+            Assert.AreEqual("Color Spray", expectedSpellModel.SpellName);
+        }
+        [TestMethod]
+        public void Confusion()
+        {
+            SpellModel expectedSpellModel = LoadSpellFileContent(HotDQ_BASE_PATH + "Confusion.txt");
+
+            Assert.AreEqual("Confusion", expectedSpellModel.SpellName);
+        }
+        [TestMethod]
+        public void Druidcraft()
+        {
+            SpellModel expectedSpellModel = LoadSpellFileContent(HotDQ_BASE_PATH + "Druidcraft.txt");
+
+            Assert.AreEqual("Confusion", expectedSpellModel.SpellName);
+        }
+        [TestMethod]
+        public void FeatherFall()
+        {
+            SpellModel expectedSpellModel = LoadSpellFileContent(HotDQ_BASE_PATH + "FeatherFall.txt");
+
+            Assert.AreEqual("Feather Fall", expectedSpellModel.SpellName);
+        }
+        [TestMethod]
+        public void InsectPlague()
+        {
+            SpellModel expectedSpellModel = LoadSpellFileContent(HotDQ_BASE_PATH + "InsectPlague.txt");
+
+            Assert.AreEqual("Insect Plague", expectedSpellModel.SpellName);
+        }
+        [TestMethod]
+        public void ScorchingRay()
+        {
+            SpellModel expectedSpellModel = LoadSpellFileContent(HotDQ_BASE_PATH + "ScorchingRay.txt");
+
+            Assert.AreEqual("Scorching Ray", expectedSpellModel.SpellName);
+        }
+        [TestMethod]
+        public void WaterWalk()
+        {
+            SpellModel expectedSpellModel = LoadSpellFileContent(HotDQ_BASE_PATH + "WaterWalk.txt");
+
+            Assert.AreEqual("Water Walk", expectedSpellModel.SpellName);
+        }
         #endregion
     }
 }
