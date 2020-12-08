83a5990b1b893d7cf908dfbaf98ad61328132a91
tstory30 <30578888+tstory30@users.noreply.github.com>
2020-12-08 06:51:17 -0700

    Updated About window, AssemblyInfo version, and MSI file version

--- FantasyModuleParser/Main/Views/About.xaml.cs
+++ FantasyModuleParser/Main/Views/About.xaml.cs
@@ -13,7 +13,7 @@ namespace FantasyModuleParser.Main
             InitializeComponent();
             // Enable it so the popup window can close on the Escape key
             PreviewKeyDown += (sender, eventArgs) => { if (eventArgs.Key == Key.Escape) Close(); };
-            versionlabel.Content = "v" + System.Windows.Forms.Application.ProductVersion + "d";
+            versionlabel.Content = "v" + System.Windows.Forms.Application.ProductVersion;
         }
         private void ESExit_Click(object sender, RoutedEventArgs e)
         {
