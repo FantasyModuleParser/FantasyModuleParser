a33ec56cafc8dab194f97de60a4fc39bcb5dd0e0
fredska <fredska@gmail.com>
2021-01-09 19:59:44 -0500

    WIP of data structure for Table option

---
+++ FantasyModuleParser/Tables/ViewModels/TableOptionViewModel.cs
@@ -0,0 +1,13 @@
+﻿using FantasyModuleParser.NPC.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FantasyModuleParser.Tables.ViewModels
+{
+    public class TableOptionViewModel : ViewModelBase
+    {
+    }
+}
