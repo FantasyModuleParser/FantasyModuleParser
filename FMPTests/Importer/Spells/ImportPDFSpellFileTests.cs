080268f96dce8ec2452b33f19fc0d4724e46e732
tstory30 <30578888+tstory30@users.noreply.github.com>
2021-01-10 17:25:11 -0700

    Updated PDF Spell Import Testing

--- FMPTests/Importer/Spells/ImportPDFSpellFileTests.cs
+++ FMPTests/Importer/Spells/ImportPDFSpellFileTests.cs
@@ -86,6 +86,8 @@ namespace FMPTests_Spells.Importer
             Assert.AreEqual(expectedSpellModel.DurationType, DurationType.Time);
             Assert.AreEqual(expectedSpellModel.DurationUnit, DurationUnit.Round);
             Assert.AreEqual(expectedSpellModel.DurationText, null);
+
+            Assert.AreEqual(expectedSpellModel.Description, "A dazzling array of flashing, colored light springs from your hand.Roll 6d10; the total is how many hit points of creatures this spell can effect. Creatures in a 15 - foot cone originating from you are affected in ascending order of their current hit points(ignoring unconscious creatures and creatures that can’t see). Starting with the creature that has the lowest current hit points, each creature affected by this spell is blinded until the spell ends.Subtract each creature’s hit points from the total before moving on to the creature with the next lowest hit points. A creature’s hit points must be equal to or less than the remaining total for that creature to be affected. At Higher Levels.When you cast this spell using a spell slot of 2nd level or higher, roll an additional 2d10 for each slot level above 1st.");
         }
         [TestMethod]
         public void Confusion()
