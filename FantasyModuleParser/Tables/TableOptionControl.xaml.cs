b5cbf89c0dc424c6ed4447b6b276327e9788a7da
fredska <fredska@gmail.com>
2021-02-08 22:45:40 -0500

    WIP on SkillUC using NPCViewModel; WIP with Dynamic Grid manipulations

--- FantasyModuleParser/Tables/TableOptionControl.xaml.cs
+++ FantasyModuleParser/Tables/TableOptionControl.xaml.cs
@@ -2,10 +2,13 @@
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows;
+using System.Windows.Data;
 
 namespace FantasyModuleParser.Tables
 {
     /// <summary>
+    /// 
+    /// 
     /// Interaction logic for TableUserControl.xaml
     /// </summary>
     public partial class TableUserControl : UserControl
@@ -32,6 +35,26 @@ namespace FantasyModuleParser.Tables
             {
                 CustomDiceRollGrid.Visibility = CustomDiceCBI.IsSelected ? Visibility.Visible : Visibility.Hidden;
                 PresetRangeGrid.Visibility = PresetRangeCBI.IsSelected ? Visibility.Visible : Visibility.Hidden;
+            }
+        }
+
+        private void DataGrid_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+            if (e.AddedCells.Count == 0)
+                this.textBox.SetBinding(TextBox.TextProperty, (string)null);
+            else
+            {
+                //var selectedCell = e.AddedCells.First();
+                var selectedCell = e.AddedCells[0];
+
+                // Assumes your header is the same name as the field it's bound to
+                var binding = new Binding(selectedCell.Column.Header.ToString())
+                {
+                    Mode = BindingMode.TwoWay,
+                    Source = selectedCell.Item,
+                    UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
+                };
+                this.textBox.SetBinding(TextBox.TextProperty, binding);
             }
         }
     }
--- FantasyModuleParser/Tables/ViewModels/TableOptionViewModel.cs
+++ FantasyModuleParser/Tables/ViewModels/TableOptionViewModel.cs
@@ -2,6 +2,7 @@
 using FantasyModuleParser.NPC.ViewModel;
 using FantasyModuleParser.Tables.Models;
 using FantasyModuleParser.Tables.Services;
+using System.Collections.ObjectModel;
 
 namespace FantasyModuleParser.Tables.ViewModels
 {
@@ -11,6 +12,9 @@ namespace FantasyModuleParser.Tables.ViewModels
         private TableModel _tableModel;
         private ModuleModel _moduleModel;
         private CategoryModel _selectedCategoryModel;
+        private ObservableCollection<ObservableCollection<CellModel>> _cells;
+        private int _gridHeight = 10;
+        private int _gridWidth = 10;
         public TableModel TableModel
         {
             get
@@ -47,9 +51,44 @@ namespace FantasyModuleParser.Tables.ViewModels
                 RaisePropertyChanged(nameof(SelectedCategoryModel));
             }
         }
+        public int GridHeight
+        {
+            get { return this._gridHeight; }
+            set
+            {
+                this._gridHeight = value;
+                RaisePropertyChanged(nameof(GridHeight));
+            }
+        }
+        private int GridWidth
+        {
+            get { return this._gridWidth; }
+            set
+            {
+                this._gridWidth = value;
+                RaisePropertyChanged(nameof(GridWidth));
+            }
+        }
         public TableOptionViewModel()
         {
             _tableService = new TableService();
+        }
+
+        private ObservableCollection<ObservableCollection<CellModel>> CreateCells()
+        {
+            var cells = new ObservableCollection<ObservableCollection<ICellViewModel>>();
+            for (var posRow = 0; posRow < GridHeight; posRow++)
+            {
+                var row = new ObservableCollection<ICellViewModel>();
+                for (var posCol = 0; posCol < GridWidth; posCol++)
+                {
+                    //var cellViewModel = new CellViewModel(Cell.Empty);
+                    //row.Add(cellViewModel);
+                }
+                cells.Add(row);
+            }
+            //return cells;
+            return null;
         }
     }
 }
