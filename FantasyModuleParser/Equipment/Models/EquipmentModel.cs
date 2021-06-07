bf5c7e0bb134d4b83f3875be7c20671a37f71f23
Fred Skallos <darkpoolgames@gmail.com>
2021-06-07 10:25:36 -0400

    Progress with Equipment Module

---
+++ FantasyModuleParser/Equipment/Models/EquipmentModel.cs
@@ -0,0 +1,31 @@
+﻿using FantasyModuleParser.Equipment.Enums;
+using FantasyModuleParser.NPC.ViewModel;
+using System.ComponentModel;
+
+namespace FantasyModuleParser.Equipment.Models
+{
+    public class EquipmentModel : ViewModelBase
+    {
+        private PrimaryEquipmentTypeEnum _primaryEquipmentType;
+        [DefaultValue(PrimaryEquipmentTypeEnum.AdventuringGear)]
+        public PrimaryEquipmentTypeEnum PrimaryEquipmentType
+        {
+            get { return _primaryEquipmentType; }
+            set { Set(ref _primaryEquipmentType, value); }
+        }
+
+        #region Secondary Enum Type Selection Options
+        private ArmorEnum _armorEnumType;
+        [DefaultValue(ArmorEnum.LightArmor)]
+        public ArmorEnum ArmorEnumType
+        {
+            get { return _armorEnumType; }
+            set { Set(ref _armorEnumType, value); }
+        }
+        #endregion Secondary Enum Type Selection Options
+
+        public EquipmentModel()
+        {
+        }
+    }
+}
---
+++ FantasyModuleParser/Equipment/UserControls/ArmorDetailsUC.xaml.cs
@@ -0,0 +1,49 @@
+﻿using FantasyModuleParser.Equipment.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FantasyModuleParser.Equipment.UserControls
+{
+    /// <summary>
+    /// Interaction logic for ArmorDetailsUC.xaml
+    /// </summary>
+    public partial class ArmorDetailsUC : UserControl
+    {
+        #region Selected Armor Enum Option
+        public static readonly DependencyProperty SelectedArmorEnumProperty =
+            DependencyProperty.Register
+            (
+                "selectedArmorEnum",
+                typeof(int),
+                typeof(ArmorDetailsUC),
+                new FrameworkPropertyMetadata(0)
+            );
+
+        public int selectedArmorEnum
+        {
+            get { 
+                return (int)GetValue(SelectedArmorEnumProperty);
+            }
+            set { 
+                SetValue(SelectedArmorEnumProperty, value); 
+            }
+        }
+        #endregion Selected Armor Enum Option
+        public ArmorDetailsUC()
+        {
+            InitializeComponent();
+        }
+    }
+}
