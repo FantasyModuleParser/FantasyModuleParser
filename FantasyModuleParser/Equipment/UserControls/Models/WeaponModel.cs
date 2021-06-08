27cc0b238b3908e79475fd516f636ba4d1798c93
fredska <fredska@gmail.com>
2021-06-08 17:21:27 -0400

    Equipment Module - Weapon Property UI work

---
+++ FantasyModuleParser/Equipment/UserControls/Models/WeaponModel.cs
@@ -0,0 +1,14 @@
+﻿using FantasyModuleParser.Equipment.Enums.Weapon;
+using FantasyModuleParser.NPC.Models.Action;
+using System.Collections.Generic;
+
+namespace FantasyModuleParser.Equipment.UserControls.Models
+{
+    public class WeaponModel
+    {
+        public DamageProperty PrimaryDamage { get; set; }
+        public DamageProperty BonusDamage { get; set; }
+
+        public List<WeaponPropertyEnum> WeaponProperties;
+    }
+}
