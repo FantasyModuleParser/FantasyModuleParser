27ca80579b1015e75d17dfc10d9f8e9126587196
tstory30 <30578888+tstory30@users.noreply.github.com>
2021-07-21 10:56:37 -0700

    Starting Exporter coding

---
+++ FantasyModuleParser/Exporters/EquipmentExporter.cs
@@ -0,0 +1,18 @@
+﻿using FantasyModuleParser.Equipment.Models;
+using FantasyModuleParser.Main.Models;
+using log4net;
+using System.Xml;
+
+namespace FantasyModuleParser.Exporters
+{
+	class EquipmentExporter
+	{
+		private static readonly ILog log = LogManager.GetLogger(typeof(EquipmentExporter));
+
+		static public string EquipmentNameToXML(EquipmentModel equipmentModel)
+		{
+			string name = equipmentModel.Name.ToLower();
+			return name.Replace(" ", "_").Replace(",", "").Replace("(", "_").Replace(")", "");
+		}
+	}
+}
