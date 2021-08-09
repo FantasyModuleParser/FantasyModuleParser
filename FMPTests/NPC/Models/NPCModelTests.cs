4880134ff5a025ff920947d659cdf9617e81104f
Fred Skallos <darkpoolgames@gmail.com>
2021-08-08 21:39:40 -0400

    Damage Type Description - Putting unit test framework together

---
+++ FMPTests/NPC/Models/NPCModelTests.cs
@@ -0,0 +1,27 @@
+﻿using FantasyModuleParser.NPC;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FantasyModuleParser.NPC.Tests
+{
+    [TestClass()]
+    public class NPCModelTests
+    {
+        [TestMethod()]
+        public void UpdateDamageVulnerabilitiesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateDamageImmunitiesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateDamageResistancesTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
