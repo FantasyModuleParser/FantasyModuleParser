d1a40015e30c874c8e7978541bfa047757935d51
tstory30 <30578888+tstory30@users.noreply.github.com>
2021-08-14 08:30:27 -0700

    Rewrote Token XML to not include line if the line will end up being <token></token>

--- FantasyModuleParser/Exporters/NPCExporter.cs
+++ FantasyModuleParser/Exporters/NPCExporter.cs
@@ -756,17 +756,13 @@ namespace FantasyModuleParser.Exporters
 		}
 		static public void WriteToken(XmlWriter xmlWriter, NPCModel npcModel, ModuleModel moduleModel)
 		{
-			xmlWriter.WriteStartElement("token");
-			xmlWriter.WriteAttributeString("type", "token");
-			if (npcModel.NPCToken == null || npcModel.NPCToken.Length < 2 || !moduleModel.IncludeTokens)
+			if (moduleModel.IncludeTokens && !npcModel.NPCToken == null && npcModel.NPCToken.Length > 2 )
 			{
-				xmlWriter.WriteString("");
-			}
-			else
-			{
+				xmlWriter.WriteStartElement("token");
+				xmlWriter.WriteAttributeString("type", "token");
 				xmlWriter.WriteValue("tokens\\" + Path.GetFileName(npcModel.NPCToken) + "@" + moduleModel.Name);
-			}
-			xmlWriter.WriteEndElement();
+				xmlWriter.WriteEndElement();
+			}			
 		}
 		static public void WriteTraits(XmlWriter xmlWriter, NPCModel npcModel)
 		{
