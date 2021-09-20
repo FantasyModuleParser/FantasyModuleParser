869ca8d597216aa714809e7f5ed6c89263b4dfda
tstory30 <30578888+tstory30@users.noreply.github.com>
2021-09-20 13:11:24 -0700

    Added IncludesClasses to ModuleModel Started work on Classes module XML

---
+++ FantasyModuleParser/Exporters/ClassExporter.cs
@@ -0,0 +1,10 @@
+﻿using FantasyModuleParser.Main.Models;
+using System.Xml;
+
+namespace FantasyModuleParser.Exporters
+{
+	class ClassExporter
+	{
+		public static void DatabaseXML_Root_Classes(XmlWriter xmlWriter, ModuleModel module)
+	}
+}
